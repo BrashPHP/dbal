e520bbdaaf396130c8660513113eb6d5fe0d65eb
Gabriel Nogueira <gabriel.nsilva@yduqs.com.br>
2025-01-02 11:35:42 -0300

    test: create connection item test suite

---
+++ tests/Pool/ConnectionItemTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Pool;
+
+use Brash\Dbal\Pool\ConnectionItem;
+use Doctrine\DBAL\Driver\Connection;
+
+test('Should assert raw connection object is received', function(){
+    $obj = new \stdClass();
+    $obj->close = fn()=> null;
+    $connMock = $this->createMock(Connection::class);
+    $connMock->expects($this->once())
+    ->method('getNativeConnection')
+    ->willReturn($obj);
+    $poolItem = new ConnectionItem($connMock);
+
+    expect($poolItem->reveal())->toBe($connMock);
+    expect($poolItem->reveal()->getNativeConnection())->toBe($obj);
+});
+
+test('Should call correct native connection on close', function(){
+    $nativeConnMock = mock();
+    $nativeConnMock->shouldReceive('close')->once();
+    $connMock = $this->createMock(Connection::class);
+    $connMock->expects($this->once())
+    ->method('getNativeConnection')
+    ->willReturn($nativeConnMock);
+    $poolItem = new ConnectionItem($connMock);
+    $poolItem->close();
+});
